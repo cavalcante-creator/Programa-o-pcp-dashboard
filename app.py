d5db01574d93c54ddc5d660612482569aaad331e
cavalcante-creator <cavalcante@colafix.com>
2026-04-13 22:14:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -274,9 +274,9 @@ async function exportarCard(produto, ordem, turno, qtde, pendente, status, data,
     pdf.text("OBSERVAÇÕES:", 10, y);
 
     y += 3;
-    pdf.rect(10, y, 190, 50);
+    pdf.rect(10, y, 190, 30);
 
-    y += 55;
+    y += 35;
 
     pdf.setFont("helvetica","bold");
     pdf.text("ASSINATURA DO OPERADOR:", 10, y);
