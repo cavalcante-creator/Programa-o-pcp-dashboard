accebc08844cb87afb795242ee1f594e17809471
cavalcante-creator <cavalcante@colafix.com>
2026-05-07 20:38:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -318,7 +318,7 @@ async function exportarCard(produto, ordem, turno, qtde, pendente, status, data,
     });
     pdf.setFont("helvetica","normal");
     y += altRow;
-    const limiteTabela = 176;
+    const limiteTabela = 170;
     while(y < limiteTabela){
         for(let j = 0; j < colunas.length; j++){
             pdf.rect(10 + j * largCol, y, largCol, altRow);
